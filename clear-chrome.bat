83a88c026f244b0abd50bdb621bcbe9741ef34ec
mariodac <38120268+mariodac@users.noreply.github.com>
2023-01-12 17:39:39 -0300

    Limpar dados navegadores

---
+++ clear-chrome.bat
@@ -0,0 +1,24 @@
+ECHO --------------------------------------
+ECHO **** Clearing Chrome cache
+taskkill /F /IM "chrome.exe">nul 2>&1
+
+set ChromeDataDir=C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default
+set ChromeCache=%ChromeDataDir%\Cache>nul 2>&1  
+set ChromeNetwork=%ChromeDataDir%\Network
+rmdir /q /s "%ChromeNetwork%
+del /q /s /f "%ChromeCache%\*.*">nul 2>&1    
+del /q /f "%ChromeDataDir%\*Cookies*.*">nul 2>&1    
+del /q /f "%ChromeDataDir%\*History*.*">nul 2>&1    
+del /q /f "%ChromeDataDir%\*Login*.*">nul 2>&1    
+
+
+set ChromeDataDir=C:\Users\%USERNAME%\Local Settings\Application Data\Google\Chrome\User Data\Default
+set ChromeCache=%ChromeDataDir%\Cache>nul 2>&1
+set ChromeNetwork=%ChromeDataDir%\Network
+rmdir /q /s "%ChromeNetwork%
+del /q /s /f "%ChromeCache%\*.*">nul 2>&1   
+del /q /f "%ChromeDataDir%\*Cookies*.*">nul 2>&1    
+del /q /f "%ChromeDataDir%\*History*.*">nul 2>&1    
+del /q /f "%ChromeDataDir%\*Login*.*">nul 2>&1    
+ECHO **** Clearing Chrome cache DONE
+PAUSE
---
+++ delete-internet-explorer.bat
@@ -0,0 +1,5 @@
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 16
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 32
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 255
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 4351
